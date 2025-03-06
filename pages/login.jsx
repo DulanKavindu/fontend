41be25bc5fe0d30cf4de957f1ef7ac179c14de24
DulanKavindu <163100627+DulanKavindu@users.noreply.github.com>
2025-03-06 10:09:31 +0530

    add a pic to the login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,8 +1,8 @@
 export default function LoginPage() {
     return (
      <div className="bg-red-400 w-full h-screen flex justify-center items-center">
-      <div className="bg-blue-600 w-[500px] h-[500px] ">
-        <img src="/images.jpg"></img>
+      <div className="bg-blue-600 w-[500px] h-[500px] flex justify-center items-center">
+        <img src="/images.jpg" className="rounded-full w-[100px] h-[100px]"></img>
 
       </div>
 
